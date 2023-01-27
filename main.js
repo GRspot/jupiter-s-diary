3a8e54cc76ed5b6b0a06f59302d58eb75d1341ac
GRspot <ymym11066@naver.com>
2023-01-27 23:30:14 +0900

    fist commit

--- main.js
+++ main.js
@@ -1,13 +1,29 @@
 let taskInput = document.getElementById("task-input");
 let addButton = document.getElementById("add-button");
+let tabs = document.querySelectorAll(".task-tabs div")
 let taskList =[]
+let mod=''
 addButton.addEventListener("click",addTask)
+console.log(tabs)
+
+for(let i=1;i<tabs.length;i++){
+    tabs[i].addEventListener("click",function(event){
+        filter(event)
+    });
+}
+
 
 function addTask(){
     let taskContent = taskInput.value;
     taskList.push(taskContent);
     console.log(taskList);
     render();
+}
+
+function filter(event){
+    mod=event.target.id
+    console.log("fliter클릭", event.target.id);
+    if(mod == "photos")
 }
 
 function render() {
